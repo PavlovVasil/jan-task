ca555f7f1f798d7bc7b0cbb4ec6c2c09f95b977d
Vasil Pavlov <vasil@vasilpavlov.com>
2021-02-12 20:59:27 +0200

    Fetch mock data from json-server

---
+++ src/Pages/Tests/Tests.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { Test } from '../../Components/Tests'
+
+export const Tests = () => {
+    const [attributes, setAttributes] = useState([])
+    const [tests, setTests] = useState([])
+
+    useEffect(() => {
+        (async() => {
+            try {
+              const urls = [
+                'http://localhost:3001/attributes/',
+                'http://localhost:3001/tests/'
+              ];
+              const allRequests = urls.map(url => fetch(url).then(res => res.json()));
+              const [
+                attributes,
+                tests
+              ] = await Promise.all(allRequests);
+
+              setAttributes(attributes);
+              setTests(tests);
+            } catch (e) {
+                console.log(e)
+            }
+        })()
+    }, [])
+    
+    return <Test />
+}
